fc7a26ddc3d10062bddbdb480da6664a53dc4b76
Giovanni Armane <giovanniarmane@gmail.com>
2017-09-07 01:43:02 -0300

    Cálculo de média e impressão de usuários

--- valemobi-batch/main.py
+++ valemobi-batch/main.py
@@ -1,6 +1,7 @@
-import sys
 import os
 import sqlite3 as sqlite
+import sys
+
 
 def database_connection_factory(database_name):
     try:
@@ -13,26 +14,6 @@ def database_connection_factory(database_name):
 
 def database_exists(database_name):
     return os.path.isfile('Data/' + database_name)
-
-
-def execute_sql(database_connection, script_path):
-    try:
-        cursor = database_connection.cursor()
-        script = open(script_path, 'r')
-        sql = script.read()
-        cursor.executescript(sql)
-        database_connection.commit()
-    except IOError:
-        print('Error: ' + script_path + ' file not found...')
-        sys.exit(1)
-    except sqlite.Error as error:
-        print('Error: %s' % error.args[0])
-        sys.exit(1)
-    finally:
-        if cursor is True:
-            cursor.close()
-        if script is True:
-            script.close()
 
 
 def table_has_data(database_connection, table_name):
@@ -53,6 +34,66 @@ def table_has_data(database_connection, table_name):
             cursor.close()
 
 
+def execute_sql(database_connection, script_path):
+    try:
+        cursor = database_connection.cursor()
+        script = open(script_path, 'r')
+        sql = script.read()
+        result = cursor.executescript(sql)
+        database_connection.commit()
+        return result
+    except IOError:
+        print('Error: ' + script_path + ' file not found...')
+        sys.exit(1)
+    except sqlite.Error as error:
+        print('Error: %s' % error.args[0])
+        sys.exit(1)
+    finally:
+        if cursor is True:
+            cursor.close()
+        if script is True:
+            script.close()
+
+
+def get_customers(database_connection):
+    try:
+        cursor = database_connection.cursor()
+        cursor.execute(
+            '''
+            select * from tb_customer_account
+            where id_customer between 1500 and 2700
+            and vl_total > 560
+            order by vl_total desc;
+            '''
+        )
+        return cursor.fetchall()
+    except sqlite.Error as error:
+        print('Error: %s' % error.args[0])
+        sys.exit(1)
+    finally:
+        if cursor is True:
+            cursor.close()
+
+
+def get_average(database_connection):
+    try:
+        cursor = database_connection.cursor()
+        cursor.execute(
+            '''
+            select avg(vl_total) from tb_customer_account
+            where id_customer between 1500 and 2700
+            and vl_total > 560;
+            '''
+        )
+        return cursor.fetchone()
+    except sqlite.Error as error:
+        print('Error: %s' % error.args[0])
+        sys.exit(1)
+    finally:
+        if cursor is True:
+            cursor.close()
+
+
 def main():
 
     db_name = r'test.db'
@@ -65,12 +106,27 @@ def main():
     try:
         print('Conectando a base de dados...')
         connection = database_connection_factory(db_name)
+
         print('Checando tabela de clientes...')
         execute_sql(connection, tb_source)
+
         if table_has_data(connection, tb_name) is False:
             print('Populando tabela de clientes...')
             execute_sql(connection, db_source)
-        print('fim')
+
+        print('Calculando média...')
+        print('Média total: {:.2f}'.format(get_average(connection)[0]))
+
+        print('Clientes usados no cálculo de média...')
+        for customer in get_customers(connection):
+            print('ID: {}, CPF/CNPJ: {}, Nome: {}, Ativo: {}, Saldo: {}'
+                  .format(
+                      customer[0],
+                      customer[1],
+                      customer[2],
+                      'Sim' if customer[3] is 1 else 'Não',
+                      customer[4]))
+
         sys.exit(0)
     except sqlite.Error as error:
         print('Error: %s' % error.args[0])
